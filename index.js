21066eec08cb7d1930ea61f0c814d7a0949bc2d0
Joe McWhorter <joseph.s.mcwhorter@gmail.com>
2019-12-19 11:48:58 -0500

    Adds basic intro materials to modify files

--- index.js
+++ index.js
@@ -1,17 +1,10 @@
 const fs = require('fs');
-
-// console.log(process.argv[2]);
+const printer= require('./printer');
 
 const fileName = process.argv[2];
 // console.log(fileName);
 
 // Error-first callback
-function printContents(error, buffer) {
-    if (error) {
-        console.log(error);
-    } else {
-        console.log(buffer.toString());
-    }
-}
 
-fs.readFile(fileName, printContents);
+
+fs.readFile(fileName, printer.printSentiment);
---
+++ printer.js
@@ -0,0 +1,31 @@
+// Importing the sentiment module from the project's node_modules directory
+const Sentiment = require(`sentiment`);
+
+// Create a new Sentiment analyzer object/instance
+const sentiment = new Sentiment();
+
+function printContents(error, buffer) {
+    if (error) {
+        console.log(error);
+    } else {
+        console.log(buffer.toString());
+    }
+}
+
+function printSentiment(error, buffer) {
+    if (error) {
+        console.log(error);
+    } else {
+        const result = sentiment.analyze(buffer.toString());
+        console.dir(result);
+    }
+}
+
+// Doesn't work because it's assignment and sentiment overwrites printContents
+// module.exports = printContents;
+// module.exports = printSentiment;
+
+module.exports = {
+    printContents: printContents,
+    printSentiment: printSentiment
+};
